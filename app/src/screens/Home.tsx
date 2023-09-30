23c64b5aca508165e93ec50a7f057325a59aa658
Paweł Dymara <p.dymara@avsystem.com>
2023-09-30 16:23:27 +0200

    add react native config

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,6 +1,9 @@
 import {Text, View} from 'react-native';
+import Config from 'react-native-config';
 
 function Home() {
+  console.log(Config.API_URL);
+
   return (
     <View>
       <Text>Siema</Text>
